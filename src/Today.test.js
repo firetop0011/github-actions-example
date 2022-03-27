2e70273fe20ffb3c21730d0f7c712d85649164ff
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-27 09:42:35 +0100

    changed text

--- src/Today.test.js
+++ src/Today.test.js
@@ -2,5 +2,5 @@ import Today from "./Today";
 
 test("App returns the right date format", () => {
   const day = Today({ date: "1992-05-27" });
-  expect(day).toBe("Today is Wednesday 27/5/1992");
+  expect(day).toBe("You are alive on Wednesday 27/5/1992");
 });
