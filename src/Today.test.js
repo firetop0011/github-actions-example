6a6efcc102fd541b3317a375ee9ceaec77df8dd7
valeriocipolla <valeriocipolla92@gmail.com>
2022-09-24 15:48:06 +0100

    fixed tests

--- src/Today.test.js
+++ src/Today.test.js
@@ -1,6 +1,6 @@
-import Today from "./Today";
+import Today from "./Today"
 
 test("App returns the right date format", () => {
-  const day = Today({ date: "1992-05-27" });
-  expect(day).toBe("You are alive on Wednesday 27/5/1992");
-});
+  const day = Today({ date: "1992-05-27" })
+  expect(day).toBe("You are alive on Wednesday 27/5/1992. Well done.")
+})
