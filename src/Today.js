7245ce83a460524fe308e836cd6caa44db344de2
valeriocipolla <valeriocipolla92@gmail.com>
2022-09-24 15:31:58 +0100

    this should break tests

--- src/Today.js
+++ src/Today.js
@@ -1,4 +1,4 @@
-const Today = (props) => {
+const Today = props => {
   const weekdays = [
     "Sunday",
     "Monday",
@@ -7,14 +7,14 @@ const Today = (props) => {
     "Thursday",
     "Friday",
     "Saturday",
-  ];
-  let date = props.date;
-  date = date ? new Date(date) : new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const currWeekDay = weekdays[date.getDay()];
-  return `You are alive on ${currWeekDay} ${day}/${month}/${year}`;
-};
+  ]
+  let date = props.date
+  date = date ? new Date(date) : new Date()
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const currWeekDay = weekdays[date.getDay()]
+  return `You are alive on ${currWeekDay} ${day}/${month}/${year}. Well done.`
+}
 
-export default Today;
+export default Today
